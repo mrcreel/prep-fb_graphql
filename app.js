aad52cec6f8a4363868bc1d3be2830ebdab89743
mrcreel <michaelrcreel@gmail.com>
2019-01-31 10:03:26 -0600

    Basic schema w/ hard coded data

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphQlHTTP = require('express-graphql')
+const {buildSchema} = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World')
-})
+app.use(
+  '/graphql',
+  graphQlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        teams: [String!]
+      }
+      schema{
+        query: RootQuery
+      }
+    `),
+    rootValue: {
+      teams: () => {
+        return ['Aberdeen', 'Adams County Christian', 'Amite']
+      }
+    },
+    graphiql: true,
+  })
+)
 
 app.listen(3000)
